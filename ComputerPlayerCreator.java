500ebb352e312fc64684909f59d9a19eb6e1b7a8
Declan Atkins <declan.atkins@ucdconnect.ie>
2017-04-19 01:35:38 +0100

    Fixed the errors with file handling

--- ComputerPlayerCreator.java
+++ ComputerPlayerCreator.java
@@ -1,5 +1,6 @@
 package poker;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Random;
 import java.io.File;
@@ -7,41 +8,48 @@ import java.io.FileNotFoundException;
 
 public class ComputerPlayerCreator {
 	
-	private Scanner sc;
 	private ArrayList<PokerPlayer> players;
 	private Random rand;
 	private int i;
-	private File file;
 	
 	public ComputerPlayerCreator(int numPlayers){
 		players = new ArrayList<PokerPlayer>();
 		rand = new Random();
-		file = new File("ListOfNames.txt");
-		try {
-			for(i=0;i<numPlayers;i++){
-				sc = new Scanner(file);
+		for(i=0;i<numPlayers;i++){
+			try {
+				File file = new File("ListOfNames.txt");
+				Scanner sc = new Scanner(file);
 				ArrayList<Integer> usedLines = new ArrayList<Integer>();
 				for(i=0;i<numPlayers;i++){
 					int pos = rand.nextInt(100);
 					while(usedLines.contains(pos)){
-						pos = rand.nextInt(99);
+						pos = rand.nextInt(100);
 					}
-					
-					skipLines(pos);
-					String name = sc.nextLine();
+					System.out.println(pos);
+					String name;
+					try{
+						skipLines(pos, sc);
+						name = sc.nextLine();
+					}
+					catch (NoSuchElementException e){
+						File file2 = new File("ListOfNames.txt");
+						Scanner sc2 = new Scanner(file2);
+						skipLines(pos, sc2);
+						name = sc2.nextLine(); 
+						sc2.close();
+					}
 					ComputerPokerPlayer next = new ComputerPokerPlayer(name);
 					players.add(next);
 				}
-			}
-		} catch (FileNotFoundException e) {
-			for(i=0;i<numPlayers;i++){
+				sc.close();
+			} catch (FileNotFoundException e) {
 				ComputerPokerPlayer next = new ComputerPokerPlayer("comp" + i);
 				players.add(next);
 			}
 		}
 	}
 	
-	private void skipLines(int numLines){
+	private void skipLines(int numLines, Scanner sc){
 		for(int i = 0;i<numLines;i++){
 			sc.nextLine();
 		}
